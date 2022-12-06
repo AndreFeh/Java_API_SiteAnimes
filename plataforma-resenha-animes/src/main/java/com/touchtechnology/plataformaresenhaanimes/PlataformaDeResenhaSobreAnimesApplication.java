f312eaf7819e2025cc57edd973c4a41ec041c059
AndreFeh <andrefelipe.c.santos@gmail.com>
2022-12-06 20:27:44 -0300

    refazendo o projeto de 07/2021, com conhecimento

---
+++ plataforma-resenha-animes/src/main/java/com/touchtechnology/plataformaresenhaanimes/PlataformaDeResenhaSobreAnimesApplication.java
@@ -0,0 +1,13 @@
+package com.touchtechnology.plataformaresenhaanimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeResenhaSobreAnimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeResenhaSobreAnimesApplication.class, args);
+	}
+
+}
---
+++ plataforma-resenha-animes/src/test/java/com/touchtechnology/plataformaresenhaanimes/PlataformaDeResenhaSobreAnimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touchtechnology.plataformaresenhaanimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeResenhaSobreAnimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
